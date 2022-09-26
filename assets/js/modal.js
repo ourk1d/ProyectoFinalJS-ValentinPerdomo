5483768d75321449feb1a9b9f3a8b4da84f0f0ed
OurKid <perdomovalentin2009@gmail.com>
2022-09-26 01:04:24 -0300

    corregi errores de estilo y otros

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -1,7 +1,10 @@
 const modalContainer = document.querySelector(".modal-container")
 const openChart = document.getElementById ("open")
+const closeChart = document.getElementById ("close")
 
 openChart.addEventListener('click', ()=>{ modalContainer.classList.toggle('active')
 })
 
 
+closeChart.addEventListener('click', ()=>{ modalContainer.classList.toggle('active')
+})
